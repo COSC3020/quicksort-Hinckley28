2b22067525f1ee56e3d6fbcc11f5b5f5bce45e50
Hinckley28 <144728931+Hinckley28@users.noreply.github.com>
2024-03-07 14:59:39 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,51 @@
-function quicksort(array) {
-    return array;
+
+function partition(arr, left, right) {
+  const pivot = arr[right];
+
+  let i = left - 1;
+
+  for (let j = left; j < right; j++) {
+    if (arr[j] <= pivot) {
+      i++;
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+
+  const temp = arr[i + 1];
+  arr[i + 1] = arr[right];
+  arr[right] = temp;
+
+  return i + 1;
 }
+
+
+function quicksort(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  const stack = [];
+  stack.push({ left, right });
+
+  while (stack.length > 0) {
+    const { left, right } = stack.pop();
+
+    if (left >= right) {
+      continue;
+    }
+
+    const pivotIndex = partition(arr, left, right);
+
+    if (pivotIndex < right) {
+      stack.push({ left: pivotIndex + 1, right });
+    }
+
+    if (pivotIndex > left) {
+      stack.push({ left, right: pivotIndex - 1 });
+    }
+  }
+
+  return arr;
+}
+  
